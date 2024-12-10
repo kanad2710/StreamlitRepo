0db2e9b60b74945c0f77459549cb3b9f287a3332
kanad2710 <147564748+kanad2710@users.noreply.github.com>
2024-12-10 21:03:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #import vega_datasets as vega
-from vega_datasets import data
 from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
